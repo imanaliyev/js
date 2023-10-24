22b88d195eb4913982ba5e537a799aa160b949aa
imanaliyev <147069258+imanaliyev@users.noreply.github.com>
2023-10-25 02:59:06 +0400

    lehnet

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// 1.prompt ilə daxil olunan ədədin , 2-nin quvveti  olub olmadigini tapan alqoritm yazin.
+// let num = +prompt("eded daxil edin")
+// for (let i = 0; i < 64; i++) {
+//     if (Math.pow(2, i) == num) {
+//        console.log(true);
+//        break
+//     }
+//     else {
+        
+//         console.log(false);
+//     }
+// }
+
+
+
+// 2.Göndərilmiş 2 ədəddən 1-cisini 2-cisinin qüvvətinə yüksəldən alqoritm yazin 
+// let num1 =  +prompt("birinci reqemi daxl edin")
+// let num2 = +prompt("ikinci reqemi daxl edin")
+// power = num1**num2
+// console.log(power);
+
+
+//3.1- den 1000e qeder  ededler icerisinde  reqemleri eyni olan ededlerin cemin tapin. example(11 ,222,333 ve  s)
+
+// let sum1 = 0
+// let sum2 = 0
+// for (let i = 0; i < 100; i++) {
+//     if (i%11 === 0 ) {
+//         sum1 = sum1 + i
+        
+        
+        
+//     }
+    
+    
+// }
+// for (let i = 100; i < 1000; i++) {
+//     if (i%111 ===0) {
+//         sum2 = sum2 +i
+
+        
+//     }
+    
+// }
+// console.log(sum1 + sum2);
+
+    
+// }
+// 4.Verilmis array-de cut ededlerin en boyuyunu tapan alqoritm yazin.
+//  const arr=[1,2,3,4,5,6,7,8,9,10,12,13,16];
+//  let result = arr[0]
+//  for (let i = 0; i < arr.length; i++) {
+//     if (result < arr[i] && arr[i] % 2 === 0) {
+//         result = arr[i]
+        
+//     }
+    
+    
+    
+//  }
+//  console.log(result);
+// 5.Verilmis array-de tek ededlerin en kiciyinin index-ni tapan alqoritm yazin.
+// const arr=[1,2,3,4,5,6,7,8,9,10,12,13,16];
+//  let result = arr[0]
+//  for (let i = 0; i < arr.length; i++) 
+//     if (result > arr[i] && arr[i] % 2 !== 0) {
+//         result = arr[i]
+//         console.log(result);
+        
+//     }
+//     console.log(result);
+
+ 
+// 6.Verilmis array-de  sade ve murekkeb ededlerin sayini tapan alqoritm yazin.
+
+const arr=[1,2,3,4,5,6,7,8,9,10,12,13,16];
+ for (let i = 0; i < arr.length; i++) {
+    if ) {
+        
+    }
+    
+ }
+
+  
